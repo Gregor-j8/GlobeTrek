d6b5ed341772f61b52f93c2a3c5a1df4ca8450b5
gregor-J8 <gregor.johnson@outlook.com>
2025-03-12 11:20:53 -0500

    added routes and AuthorizedViews

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Login } from './components/auth/Login'
 import { Register } from './components/auth/Register'
+import { Authorized } from './views/Authorized'
+import { AuthorizedViews } from './views/AuthorizedViews'
 
 
 function App() {
   return (
     <>
-      <Login />
-      <Register />
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route 
+          path='*'
+          element={
+            <Authorized>
+                <AuthorizedViews />
+            </Authorized>
+          }
+        />
+      </Routes>
     </>
-  )
-}
+  )}
 
 export default App
