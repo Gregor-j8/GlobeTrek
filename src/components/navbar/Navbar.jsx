a62d43f226db395a446021eccd661f125cb28f2a
gregor-J8 <gregor.johnson@outlook.com>
2025-03-18 16:11:27 -0500

    added map

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ export const Navbar = ({currentUser}) => {
             <ul className="bg-main-color flex justify-evenly p-4 fixed w-full top-0">
                 <Link to={"/"}><li className="text-color-primary">Home</li></Link>
                 <Link to={"/posts"}><li className="text-color-primary">Posts</li></Link>
+                <Link to={"/map"}><li className="text-color-primary">World Map</li></Link>
                 <Link to={"/favorites"}><li className="text-color-primary">Favorites</li></Link>
                 <Link to={`/profile/${currentUser.id}`}><li className="text-color-primary">Profile</li></Link>
                 {localStorage.getItem("user") ? (
