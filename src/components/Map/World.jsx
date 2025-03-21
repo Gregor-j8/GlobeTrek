2c49887eb33ea3a2724700ea1704bc6d82e13508
gregor-J8 <gregor.johnson@outlook.com>
2025-03-20 20:53:03 -0500

    dark Mode map

--- src/components/Map/World.jsx
+++ src/components/Map/World.jsx
@@ -41,11 +41,11 @@ export const World = () => {
 
 
   return (
-      <div className="h-screen w-[100%] ">
+      <div className="h-screen w-[100%]">
             <MapContainer  center={[0.505, -0.09]} zoom={3} minZoom={3} maxZoom={19} maxBounds={bounds} maxBoundsViscosity={1.0}
-            style={{  height: "100vh", width: "100%", position: "absolute", bottom: 0 }}
+            style={{ height: "100vh", width: "100%", position: "absolute", bottom: 0 }}
               whenCreated={(mapInstance) => (mapRef.current = mapInstance)}>
-                <div className=" top-2.5 left-[25%] text-xl z-[10000] absolute">
+                <div className=" top-14 left-[25%] text-xl z-[10000] absolute">
                    <button className="bg-blue-500 text-white px-4 py-2 rounded shadow-md hover:bg-blue-700 transition z-[10000]" onClick={() => SetNewPostModal(true)}>New Post</button>
                     <button className="bg-blue-500 text-white px-4 py-2 rounded shadow-md hover:bg-blue-700 transition z-[10000]"  onClick={() => SetUserPost(true)}>user Marker</button>
                     <button className="bg-blue-500 text-white px-4 py-2 rounded shadow-md hover:bg-blue-700 transition z-[10000]" onClick={() => SetUserPost(false)}>See all Marker</button>
@@ -59,8 +59,10 @@ export const World = () => {
                       )}
           <TileLayer
           maxZoom={22}
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+           //LightMode Url url="https://mt1.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}"
+          //  attribution='&copy; <a href="https://maps.google.com/">Google Maps</a>'
           noWrap={true}
         />
         {markers.map(marker => {
