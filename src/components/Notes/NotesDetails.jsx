0c00e460973c632207ff0ac65ebc3681fad46be8
gregor-J8 <gregor.johnson@outlook.com>
2025-03-26 09:39:20 -0500

    added details for notes

---
+++ src/components/Notes/NotesDetails.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { GetUserNotes } from "../../services/NotesService"
+
+export const NotesDetails = () => {
+    const { notesId } = useParams()
+    const navigate = useNavigate()
+    const [notesDetails, setNotesDetails] = useState({})
+
+    useEffect(() => {
+        GetUserNotes(notesId).then(data => {
+            const post = data[0]
+            setNotesDetails(post)
+        })}, [notesId])
+
+    return (
+        <div className="w-full h-screen flex  items-center justify-center ">
+            <div className="flex flex-col bg-main-card p-10">
+                <section className=" flex flex-col justify-between">
+                    <Link to={`/profile/${notesDetails}`}>
+                        <h1 className="flex justify-center text-color-primary">{notesDetails.toVisit}</h1>
+                    </Link> 
+                    <h2 className="flex pt-10 text-color-primary">{notesDetails.toDoList}</h2>
+                </section>
+                    <p className="items-center text-color-primary p-15"></p>
+                    <footer className="flex flex-col text-l p-2">
+                        <div className="flex justify-between">
+                            <button className="cursor-pointer text-color-primary p-2 button-primary" onClick={() => {navigate("edit")}}>Edit</button> 
+                            <button className="cursor-pointer text-color-primary p-2 button-primary" onClick={() => {navigate("/notes")}}>return</button> 
+                            <button className="button-primary cursor-pointer p-2 text-color-primary" value={notesDetails.id}>Delete</button>
+                        </div>
+                    </footer>
+            </div>
+        </div>
+    )
+}
