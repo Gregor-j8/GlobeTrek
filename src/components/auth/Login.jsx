1a91499d9a774b42c421282292d025c442983e87
gregor-J8 <gregor.johnson@outlook.com>
2025-03-30 13:16:21 -0500

    added styles to register

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -27,10 +27,10 @@ export const Login = () => {
   }
 
   return (
-  <div className="min-h-screen flex flex-col justify-center">
-    <div className=" flex flex-col p-10 w-1/3 mx-auto md:w-full md:max-w-md">
-      <h1 className="font-bold text-center text-2xl mb-5 text-white">GlobeTrek</h1>  
-      <form className="bg-white shadow w-full rounded-lg" onSubmit={handleLogin}>
+  <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="font-bold text-center text-2xl mb-5 text-white">GlobeTrek</h1>
+      <div className="flex flex-col p-10 w-full min-w-[200px] lg:w-1/3  mx-auto">
+      <form className="bg-white shadow w-full rounded-lg items-center" onSubmit={handleLogin}>
         <fieldset className="flex px-5 py-7">
           <label className="font-semibold  text-gray-600 pb-2 mt-1 mr-2">E-mail:</label>
           <input type="email"
@@ -44,7 +44,7 @@ export const Login = () => {
             <div className="flex items-center justify-center m-3 ">
           <button type="submit"
           className="flex bg-blue-500 hover:bg-blue-600 text-white w-1/3 items-center 
-          justify-center py-2.5 rounded-lg text-sm font-semibold text-center">Login</button>
+          justify-center py-2.5 rounded-lg text-sm font-semibold text-center cursor-pointer">Login</button>
             </div>
         </form>
       <div className="py-5">
