8c3b36a48ae0b6e53990a559e9aa8ab07a81baa8
gregor-J8 <gregor.johnson@outlook.com>
2025-04-04 08:22:55 -0500

    added tests for follow modals

---
+++ __tests__/Follow/FollowerModal.test.jsx
@@ -0,0 +1,38 @@
+import "@testing-library/jest-dom"
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { FollowerModal } from '../../src/components/Follow/FollowerModal'
+import { vi, describe, it, expect, beforeEach } from "vitest"
+
+
+describe('FollowerModal', () => {
+    const mockOnClose = vi.fn()
+    const mockFollowers = [
+        {
+            user: {
+                id: '1',
+                fullName: 'g'
+            }
+        },
+        {
+            user: {
+                id: '2',
+                fullName: 'j'
+            }
+        }
+    ]
+
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <FollowerModal follower={mockFollowers} onClose={mockOnClose} />
+            </MemoryRouter>
+        )
+    })
+
+    it('renders follower names', () => {
+        expect(screen.getByText('g')).toBeInTheDocument()
+        expect(screen.getByText('j')).toBeInTheDocument()
+    })
+
+})
